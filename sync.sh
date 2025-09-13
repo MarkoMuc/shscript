23fc5969cf1ad0c316806851d747450b2d0a66d4
Marko Z. Muc <marko.muc12@gmail.com>
2025-09-13 17:28:51 +0200

    added more logging to lib sync

--- sync.sh
+++ sync.sh
@@ -11,4 +11,6 @@ rsync -rvhP \
   --exclude "sync.sh" \
   ~/Books/ alexandria:~/Books
 
-nohup ssh alexandria "mkdir -p /home/alexandria/Archive && zip -r /home/alexandria/Archive/books_\$(date +%V)_\$(date +%Y).zip /home/alexandria/Books" >> "$LOGFILE" 2>&1 &
+nohup ssh alexandria \
+  "mkdir -p /home/alexandria/Archive && zip -r /home/alexandria/Archive/books_\$(date +%V)_\$(date +%Y).zip /home/alexandria/Books >> /home/alexandria/archive.logs 2>&1" \
+  >> "$LOGFILE" 2>&1 &
